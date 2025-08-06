8bd26f2003adeb9317bdad43246c8de97cd0fef7
Kazybek <kazybeknurlanbek@gmail.com>
2025-08-06 23:44:56 +0200

    Add seeds for movies, books, and recipes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,41 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Movie.create([
+  { title: 'Sister without drive' },
+  { title: 'Almost buy statement' },
+  { title: 'Under common' },
+  { title: 'Skill design lay' },
+  { title: 'Girl home share' },
+  { title: 'Toward produce' },
+  { title: 'Can hour recently finish' },
+  { title: 'Smile possible' },
+  { title: 'Nor' },
+  { title: 'Matter crime charge' }
+])
+
+Book.create([
+  { title: 'System value employee player record' },
+  { title: 'Up your threat easy letter' },
+  { title: 'Laugh single another' },
+  { title: 'Surface choice' },
+  { title: 'Performance only century radio' },
+  { title: 'Campaign check' },
+  { title: 'Political room international tough' },
+  { title: 'Evening which' },
+  { title: 'Marriage improve' },
+  { title: 'Wonder theory' }
+])
+
+Recipe.create([
+  { title: 'Together bad series another' },
+  { title: 'Participant side dream home' },
+  { title: 'Approach some answer sit cup my' },
+  { title: 'Behavior executive production suffer' },
+  { title: 'Skin lead term too' },
+  { title: 'Ready light ten late' },
+  { title: 'Management dream kid' },
+  { title: 'Scene speak decide exactly wrong half' },
+  { title: 'By beyond material among foot' },
+  { title: 'Who kid house over' }
+])
