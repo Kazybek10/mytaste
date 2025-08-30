349b4e50bc431f0def39b14a5d91829d4fbd3671
Kazybek <kazybeknurlanbek@gmail.com>
2025-08-30 23:41:15 +0200

    fix:pages. add:forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'home/index'
-  get 'recipes/index'
-  get 'recipes/show'
-  get 'recipes/new'
-  get 'recipes/create'
-  get 'recipes/edit'
-  get 'recipes/update'
-  get 'recipes/destroy'
-  get 'books/index'
-  get 'books/show'
-  get 'books/new'
-  get 'books/create'
-  get 'books/edit'
-  get 'books/update'
-  get 'books/destroy'
-  get 'movies/index'
-  get 'movies/show'
-  get 'movies/new'
-  get 'movies/create'
-  get 'movies/edit'
-  get 'movies/update'
-  get 'movies/destroy'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  devise_for :users
 
   resources :movies
   resources :books
   resources :recipes
 
-  # Defines the root path route ("/")
-  root "home#index"
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create]
+  delete "/logout", to: "sessions#destroy", as: :logout
+  get "/signup", to: "users#new", as: :signup
+  get "/login", to: "sessions#new", as: :login
 
-  # root "posts#index"
+  root "home#index"
 end
