d55eafca0e5ad53235d3470146512a25c9a79b91
Kazybek <kazybeknurlanbek@gmail.com>
2026-04-13 16:27:19 +0200

    Redesign home page, simplify logo, fix navbar search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,13 +3,9 @@ class SearchController < ApplicationController
     query = params[:query].to_s.strip
 
     if query.length >= 2
-      base_movies  = user_signed_in? ? current_user.movies  : Movie.all
-      base_books   = user_signed_in? ? current_user.books   : Book.all
-      base_recipes = user_signed_in? ? current_user.recipes : Recipe.all
-
-      @movies  = base_movies.where("title ILIKE ?",  "%#{query}%").limit(4)
-      @books   = base_books.where("title ILIKE ?",   "%#{query}%").limit(4)
-      @recipes = base_recipes.where("title ILIKE ?", "%#{query}%").limit(4)
+      @movies  = Movie.where("title ILIKE ?",  "%#{query}%").limit(4)
+      @books   = Book.where("title ILIKE ?",   "%#{query}%").limit(4)
+      @recipes = Recipe.where("title ILIKE ?", "%#{query}%").limit(4)
     else
       @movies = @books = @recipes = []
     end
